c428f5dffa6caaf105d6ecf7c6242448800f2261
Vo Minh Long <vominhlongbentre@gmail.com>
2025-01-15 23:17:47 +0700

    final

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,16 @@
 import type { PageServerLoad } from "./$types";
-import fs from 'fs';
+import process from "process";
 import path from "path";
+import fs from "fs";
 
 export const load = (async() => {
 	let fileNames: string[] = [];
-	const files = fs.readdirSync(path.resolve('src/lib/audio'), { withFileTypes: true });
+	const dir = path.join(process.cwd(), "/src/lib/audio");
+	console.log(dir);
+	const files = fs.readdirSync(dir, { withFileTypes: true });
 	for (const file of files) {
 		if (file.isFile()) {
-				fileNames.push(file.name);
+			fileNames.push(file.name);
 		}
 	}
 	return {
